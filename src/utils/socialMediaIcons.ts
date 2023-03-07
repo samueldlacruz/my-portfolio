f546330c54fb469f354998c33a08ebe37bbf59df
Samuel De la Cruz <s.delacruz@tele.finance>
2023-03-07 14:05:39 -0400

    add: social media icons helper added

---
+++ src/utils/socialMediaIcons.ts
@@ -0,0 +1,7 @@
+import linkedinIconSrc from "../assets/social-media/linkedin.png";
+import githubIconSrc from "../assets/social-media/github.png";
+
+export const socialMediaIcons: { [key: string]: string } = {
+    "github": githubIconSrc,
+    "linkedin": linkedinIconSrc
+}
